aa991a5bb673df844e89be8807a282ff979a763a
Ibrahim Alshubaily <ibrahim.alshubaily.sde@gmail.com>
2023-02-26 20:16:17 +0000

    min max scaffold

--- chess.cpp
+++ chess.cpp
@@ -6,16 +6,24 @@
 #include <vector>
 using namespace std;
 
-struct Position { 
+struct Position
+{
     int row, col;
     bool const operator<(const Position &other) const {
         return row < other.row || (row == other.row && col < other.col);
     }
 };
 
+struct Move {
+    Position source, destination;
+};
+
 struct Direction {
     int rowOffset, colOffset;
 };
+
+int max(map<Position, char> pieces, map<char, vector<Direction>> piecesDirections, map<char, int> piecesNumSteps,  map<char, int> piecesScores, bool blackTurn, int depth);
+int min(map<Position, char> pieces, map<char, vector<Direction>> piecesDirections, map<char, int> piecesNumSteps,  map<char, int> piecesScores, bool blackTurn, int depth);
 
 map<Position, char> fenToMap() {
     map<Position, char> output;
@@ -46,6 +54,18 @@ map<char, int> getPieceNumSteps() {
     return output;
 }
 
+
+map<char, int> getPieceHeuristicScore() {
+    map<char, int> output;
+    output['p'] = 1;
+    output['n'] = 3;
+    output['b'] = 3;
+    output['r'] = 5;
+    output['q'] = 15;
+    output['k'] = 100000;
+    return output;
+}
+
 map<char, vector<Direction>> getPieceDirections() {
     map<char, vector<Direction>> output;
     
@@ -62,6 +82,7 @@ map<char, vector<Direction>> getPieceDirections() {
     output['k'] = queenDirections;
     return output;
 }
+
 set<Position> getPawnMoves(Position piecesPosition, vector<Direction> pieceDirections, int numSteps, map<Position, char> pieces) {
     set<Position> output;
     char piece = pieces[piecesPosition];
@@ -122,22 +143,20 @@ set<Position> getMoves(Position piecesPosition, vector<Direction> pieceDirection
     return output;
 }
 
-map<Position, set<Position>> getAllMoves( map<Position, char> pieces, map<char, vector<Direction>> piecesDirections, map<char, int> piecesNumSteps) {
+map<Position, set<Position>> getAllMoves( map<Position, char> pieces, map<char, vector<Direction>> piecesDirections, map<char, int> piecesNumSteps, bool blackTurn) {
     map<Position, set<Position>> output;
-    int numMoves = 0;
+    // int numMoves = 0;
     for (auto const& [position, piece] : pieces){
-        set<Position> moves = getMoves(position, 
-        piecesDirections[tolower(piece)], 
-        piecesNumSteps[tolower(piece)],
-         pieces);
+        if (islower(piece) == blackTurn) {
+            set<Position> moves = getMoves(position, 
+            piecesDirections[tolower(piece)], 
+            piecesNumSteps[tolower(piece)],
+            pieces);
 
-        output[position] = moves;
-        numMoves += moves.size();
-        // for (Position dest: moves) {
-        //     cout << "Possible move (row, col) -> (row, col): " << position.row << ", " << position.col << " -> " << dest.row << ", " << dest.col << "\n";
-        // }
+            output[position] = moves;
+        }
     }
-    cout << "NUM MOVES: " << numMoves << "\n";
+    // cout << "NUM MOVES: " << numMoves << "\n";
     return output;
 }
 
@@ -155,35 +174,139 @@ void display_board(map<Position, char> pieces) {
     cout << "\n\n";
 }
 
+int eval(map<Position, char> pieces, map<char, int> piecesScores, bool blackTurn) {
+    int score = 0, otherScore = 0;
+    for (auto const& [position, piece] : pieces){
+        if (islower(piece) == blackTurn) {
+            score += piecesScores[tolower(piece)];
+        } else {
+            otherScore += piecesScores[tolower(piece)];
+        }
+    }
+    // cout << score << " - " << otherScore << "\n";
+    return score - otherScore;
+}
+
+int max(map<Position, char> pieces, map<char, vector<Direction>> piecesDirections, map<char, int> piecesNumSteps, map<char, int> piecesScores, bool blackTurn, int depth) {
+    if (depth == 0) {
+        return eval(pieces, piecesScores, blackTurn);
+    }
+    int maxScore = -1000000;
+    for (auto const& [source, destinations] : getAllMoves(pieces, piecesDirections, piecesNumSteps, blackTurn)){
+        for (auto const& destination : destinations){
+            char *prev_destination_piece = &pieces[destination];
+            pieces[destination] = pieces[source];
+            pieces.erase(source);
+            
+            int score = min(pieces, piecesDirections, piecesNumSteps, piecesScores, blackTurn, depth - 1);
+            if (score > maxScore) {
+                maxScore = score;
+            }
+
+            pieces[source] = pieces[destination];
+            if (prev_destination_piece != NULL) {
+                pieces.erase(destination);
+            } else {
+                pieces[destination] = *prev_destination_piece;
+            }
+        }
+    }
+    return maxScore;
+}
+
+int min(map<Position, char> pieces, map<char, vector<Direction>> piecesDirections, map<char, int> piecesNumSteps, map<char, int> piecesScores, bool blackTurn, int depth) {
+    if (depth == 0) {
+        return eval(pieces, piecesScores, blackTurn);
+    }
+    int minScore = 1000000;
+    for (auto const& [source, destinations] : getAllMoves(pieces, piecesDirections, piecesNumSteps, !blackTurn)){
+        for (auto const& destination : destinations){
+            char *prev_destination_piece = &pieces[destination];
+            pieces[destination] = pieces[source];
+            pieces.erase(source);
+            
+            int score = max(pieces, piecesDirections, piecesNumSteps, piecesScores, blackTurn, depth - 1);
+            if (score < minScore) {
+                minScore = score;
+            }
+
+            pieces[source] = pieces[destination];
+            if (prev_destination_piece != NULL) {
+                pieces.erase(destination);
+            } else {
+                pieces[destination] = *prev_destination_piece;
+            }
+        }
+    }
+    return minScore;
+}
+
+
+Move get_best_move(map<Position, char> pieces, map<char, vector<Direction>> piecesDirections, map<char, int> piecesNumSteps, map<char, int> piecesScores, bool blackTurn) {
+    Move bestMove;
+    int maxScore = -1000000;
+    for (auto const& [source, destinations] : getAllMoves(pieces, piecesDirections, piecesNumSteps, blackTurn)){
+        for (auto const& destination : destinations){
+            char *prev_destination_piece = &pieces[destination];
+            pieces[destination] = pieces[source];
+            pieces.erase(source);
+
+            int score = 0; // min(pieces, piecesDirections, piecesNumSteps, piecesScores, blackTurn, 0);
+            if (score > maxScore) {
+                cout << score << "\n";
+                maxScore = score;
+                bestMove = {source, destination};
+            }
+
+            pieces[source] = pieces[destination];
+            if (prev_destination_piece != NULL) {
+                pieces.erase(destination);
+            } else {
+                pieces[destination] = *prev_destination_piece;
+            }
+        }
+    }
+    return bestMove;
+}
+
+
 int main() {
     map<Position, char> pieces = fenToMap();
     map<char, int> piecesNumSteps = getPieceNumSteps();
     map<char, vector<Direction>> piecesDirections = getPieceDirections();
+    map<char, int> piecesScores = getPieceHeuristicScore();
 
-    while (true) {
+    display_board(pieces);
+    bool blackTurn = true;
+    int nSteps = 5;
+    while (nSteps-- > 0) {
+        Move bestMove = get_best_move(pieces, piecesDirections, piecesNumSteps, piecesScores, blackTurn);
+        pieces[bestMove.destination] = pieces[bestMove.source];
+        pieces.erase(bestMove.source);
+        blackTurn = !blackTurn;
         display_board(pieces);
-        cout << "Enter Source (row col): ";
-        string input;
-        cin >> input;
-        Position source = {int(input.at(0)) - 48, int(input.at(1)) - 48};
-        cout << "Selected (row: " << source.row << ", col: " << source.col << ")\n";
 
-        char piece = tolower(pieces[source]);
-        set<Position> moves = getMoves(source, piecesDirections[piece], piecesNumSteps[piece], pieces);
-        cout << "num possible moves: " << moves.size() << "\n";
-        for (Position position: moves) {
-            cout << "Possible move (row, col) : " << position.row << ", " << position.col << "\n";
-        }
-        cout << "Enter destination (row col):" << "\n";
-        input = "";
-        cin >> input;
-        Position destination = {int(input.at(0)) - 48, int(input.at(1)) - 48};
-        if (moves.contains(destination)) {
-            pieces[destination] = pieces[source];
-            pieces.erase(source);
-        }
-        getAllMoves(pieces, piecesDirections, piecesNumSteps);
+                // cout << "Enter Source (row col): ";
+        // string input;
+        // cin >> input;
+        // Position source = {int(input.at(0)) - 48, int(input.at(1)) - 48};
+        // cout << "Selected (row: " << source.row << ", col: " << source.col << ")\n";
 
+        // char piece = tolower(pieces[source]);
+        // set<Position> moves = getMoves(source, piecesDirections[piece], piecesNumSteps[piece], pieces);
+        // cout << "num possible moves: " << moves.size() << "\n";
+        // for (Position position: moves) {
+        //     cout << "Possible move (row, col) : " << position.row << ", " << position.col << "\n";
+        // }
+        // cout << "Enter destination (row col):" << "\n";
+        // input = "";
+        // cin >> input;
+        // Position destination = {int(input.at(0)) - 48, int(input.at(1)) - 48};
+        // if (moves.contains(destination)) {
+        //     pieces[destination] = pieces[source];
+        //     pieces.erase(source);
+        // }
+        // getAllMoves(pieces, piecesDirections, piecesNumSteps);
     }
     return 1;
 }
